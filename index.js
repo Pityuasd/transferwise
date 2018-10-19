39000c01d93feca1571336e8627fc3b9d8374dfa
Zsolt Fekete <qzephyrmor@gmail.com>
2018-10-20 01:15:37 +0200

    Sending test transfer data via /post endpoint

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 // Including config file
+var TransferSchema = {
+    name: "Transfer",
+    type: "record",
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "src_currency",
+            type: "string"
+        },
+        {
+            name: "profile_id",
+            type: "string"
+        },
+        {
+            name: "tgt_currency",
+            type: "string"
+        },
+        {
+            name: "source_amount",
+            type: "string"
+        },
+        {
+            name: "recipient_id",
+            type: "string"
+        },
+        {
+            name: "submit_time",
+            type: "double"
+        }
+    ]
+};
+
+var avsc = require('avsc');
+var TransferSchemaType = avsc.parse(TransferSchema);
+
 const config = require("./config.json");
 const express = require("express");
 const app = express();
@@ -7,6 +45,8 @@ const port = config.application.port || 3000;
 
 // Kafka stuffs
 var HighLevelConsumer = kafka.HighLevelConsumer;
+var HighLevelProducer = kafka.HighLevelProducer;
+var KeyedMessage = kafka.KeyedMessage;
 var Client = kafka.Client;
 
 // Creating a new Kafka client
@@ -18,7 +58,7 @@ var client = new Client(config.kafka.host, config.kafka.clientId, {
 
 // Handling Kafka client errors; it will be displayed only on the standard error output.
 client.on("error", function (error) {
-    console.error(error);
+    console.log(error);
 });
 
 var consumer = new HighLevelConsumer(
@@ -58,6 +98,47 @@ app.use(express.static("public"));
 
 app.get("/", function (request, response) {
     response.send("Na mizu");
+});
+
+app.get("/post", function (request, response) {
+    var producer = new HighLevelProducer(client);
+
+    producer.on("ready", function () {
+        // Create message and encode to Avro buffer
+        var messageBuffer = TransferSchemaType.toBuffer({
+                id: "1",
+                src_currency: "EUR",
+                profile_id: "978",
+                tgt_currency: "RUB",
+                source_amount: "58412",
+                recipient_id: "879",
+                submit_time: Date.now(),
+            });
+
+        // Create a new payload
+        var payload = [{
+            topic: 'node-test',
+            messages: messageBuffer,
+            attributes: 1
+        }];
+
+        //Send payload to Kafka and log result/error
+        producer.send(payload, function (error, result) {
+            console.info('Sent payload to Kafka: ', payload);
+            if (error) {
+                console.error(error);
+            } else {
+                var formattedResult = result[0];
+                console.log('result: ', result)
+            }
+        });
+    });
+
+    producer.on("error", function (error) {
+        console.error(error);
+    });
+
+    response.send("sent");
 });
 
 app.listen(port, function () {
