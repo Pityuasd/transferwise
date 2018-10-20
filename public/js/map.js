53af97e5b2616ac9f49489acb899455410346bdb
nandi <nandor.z.vigh@gmail.com>
2018-10-20 20:24:54 +0200

    zsoltibuzi

--- public/js/map.js
+++ public/js/map.js
@@ -36,12 +36,79 @@ window.TransferWise = window.TransferWise || {};
             container: 'map',
             style: 'mapbox://styles/hitertamas/cjnhlyzz85g642rsc4f4p8hem',
             center: [30, 26],
-            zoom: 1.4,
+            zoom: 1.5,
             position: 'fixed',
-            minZoom: '0'
+            minZoom: '0',
+            interactive: false,
+            maxBounds: [
+                [-180, -90],
+                [180, 90]
+            ]
         });
+
+        map.on('load', function () {
+            map.addSource('states', {
+                'type': 'geojson',
+                'data': 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'
+            });
+
+            map.addLayer({
+                'id': 'states-layer',
+                'type': 'fill',
+                'source': 'states',
+                'paint': {
+                    "fill-outline-color": "rgba(0,0,0,0.1)",
+                    "fill-color": "rgba(0,0,0,0.1)"
+                }
+            })
+
+            map.addLayer({
+                'id': 'states-highlited',
+                'type': 'fill',
+                'source': 'states',
+                'paint': {
+                    "fill-outline-color": "#00b9ff",
+                    "fill-color": "#00b9ff",
+                    "fill-opacity": 1
+                },
+                'filter': ["in", "geounit", ""]
+            })
+
+            map.addLayer({
+                'id': 'states-selected',
+                'type': 'fill',
+                'source': 'states',
+                'paint': {
+                    "fill-color": "#787878",
+                    "fill-opacity": 0.5
+                },
+                'filter': ["in", "geounit", ""]
+            })
+
+            map.on('click', function(e){
+
+                // $("#exampleModal").modal()
+
+                map.getCanvas().style.cursor = 'pointer';
+
+                features = map.queryRenderedFeatures(e.point, { layers: ['states-layer'] });
+                var feature = features[0];
+
+                map.setFilter('states-highlited', ['in', 'geounit', feature.properties.geounit]);
+            });
+
+            map.on('mousemove', function(e){
+                features = map.queryRenderedFeatures(e.point, { layers: ['states-layer'] });
+                var feature = features[0];
+
+                map.setFilter('states-selected', ['in', 'geounit', feature.properties.geounit]);
+            });
+
+        });
+
         return map
     }
+
 
     handler.map = createMap();
 
