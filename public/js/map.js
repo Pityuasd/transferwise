748a0ddb60f815d362fb8448d9518f97a4450965
nandi <nandor.z.vigh@gmail.com>
2018-10-20 16:41:04 +0200

    navbar

--- public/js/map.js
+++ public/js/map.js
@@ -19,18 +19,28 @@ window.TransferWise = window.TransferWise || {};
      *
      */
     function createMap() {
+        // mapboxgl.accessToken = 'pk.eyJ1IjoicGp0dXhlIiwiYSI6ImNqbmdqNjF5MTAwcmEzcXA3aWQxa21objcifQ.QuNhQ-rc9Q3KwB6GHQCTPg';
+        // return new mapboxgl.Map({
+        //     container: container,
+        //     // style: "mapbox://styles/pjtuxe/cjngjnuvi0mom2rsqrq3u4sl5",
+        //     style: "mapbox://styles/kapitanytms/cjlgbkiev2eg52rnfiz9kmcka", //hosted style id
+        //     center: [60, 26], // starting position
+        //     zoom: 1, // starting zoom
+        //     maxBounds: [
+        //         [-180, -90],
+        //         [180, 90]
+        //     ]
+        // });
         mapboxgl.accessToken = 'pk.eyJ1IjoicGp0dXhlIiwiYSI6ImNqbmdqNjF5MTAwcmEzcXA3aWQxa21objcifQ.QuNhQ-rc9Q3KwB6GHQCTPg';
-        return new mapboxgl.Map({
-            container: container,
-            // style: "mapbox://styles/pjtuxe/cjngjnuvi0mom2rsqrq3u4sl5",
-            style: "mapbox://styles/kapitanytms/cjlgbkiev2eg52rnfiz9kmcka", //hosted style id
-            center: [60, 26], // starting position
-            zoom: 0, // starting zoom
-            maxBounds: [
-                [-180, -90],
-                [180, 90]
-            ]
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/dark-v9',
+            // center: [60, 26],
+            zoom: 1.5,
+            position: 'fixed',
+            minZoom: '0'
         });
+        return map
     }
 
     handler.map = createMap();
