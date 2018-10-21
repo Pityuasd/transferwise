6eb0c878618b25ae9cc696c6452651e2c8999c42
Zsolt Fekete <qzephyrmor@gmail.com>
2018-10-21 11:49:33 +0200

    Tutorial JS refinements

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -1,51 +1,32 @@
 /**
  *
  */
-function setPositionTutorialModal(id) {
-    $(id).css("top",  $(window).width() * 0.145);
-    $(id).css("left", $(window).height() * -1.7);
-}
 
-function startTutorial() {
-    $(window).on('load',function() {
-        $('#openModalTutorial').modal('show');
-    });
-
-    $(window).ready(function(){
-        $("#startTutorial").click(function(){
-            $("#tutorialModalStepFirst").modal('show');
-        });
-    });
-
-    $(window).ready(function(){
+var startTutorial = (function () {
+    $(window).ready(function () {
         $("#nextSecondTutorialStep").click(function(){
             $("#tutorialModalStepSecond").modal('show');
         });
-    });
 
-    $(window).ready(function(){
         $("#nextThirdTutorialStep").click(function(){
             $("#tutorialModalStepThird").modal('show');
         });
-    });
 
-    $(window).ready(function(){
         $("#nextFourthTutorialStep").click(function(){
             $("#tutorialModalStepFourth").modal('show');
         });
-    });
 
-    $(window).ready(function(){
         $("#nextFifthTutorialStep").click(function(){
             $("#tutorialModalStepFifth").modal('show');
         });
-    });
 
-    $(window).ready(function(){
         $("#nextSixthTutorialStep").click(function(){
             $("#tutorialModalStepSixth").modal('show');
         });
     });
-}
+})();
 
-startTutorial();
+var setPositionTutorialModal = function (id) {
+    // $(id).css("top", $(window).height() / 2);
+    // $(id).css("left", $(window).width() - 280);
+};
